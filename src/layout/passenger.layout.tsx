ebaf8c5176ef89f15bd5ab14b6704ad3441b4d24
Vincent#05 <vincentobenza2003@gmail.com>
2025-06-17 19:40:54 +0800

    Added hero component

---
+++ src/layout/passenger.layout.tsx
@@ -0,0 +1,84 @@
+import { NavLink, Outlet } from "react-router-dom";
+import NavbarBackground from "../assets/navbar-background.jpg";
+
+export default function PassengerLayout() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow bg-gray-900 ">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+const Navbar = () => {
+  const list = [
+    {
+      name: "Terms and Conditions",
+      link: "/terms-and-conditions",
+    },
+    {
+      name: "About Us",
+      link: "/about-us",
+    },
+    {
+      name: "Contact Us",
+      link: "/contact-us",
+    },
+  ];
+  return (
+    <div className="w-full bg-green-600 h-20 flex items-center justify-between px-2 md:px-12 relative">
+      {/* background image */}
+      <div
+        className="absolute inset-0 bg-cover bg-center opacity-30 "
+        style={{
+          backgroundImage: `url(${NavbarBackground})`,
+        }}
+      />
+      {/* logo */}
+      <div className="flex items-center gap-4 text-white z-5">
+        <img
+          src="https://cdn-icons-png.flaticon.com/128/2878/2878877.png"
+          alt="Logo"
+          className="h-10 w-10 mr-2"
+        />
+        <div className="flex flex-col gap-2 font-bold">
+          <h1>
+            CENAMARK <br />
+            SHIPPING LINES
+          </h1>
+        </div>
+      </div>
+
+      <ul className="flex items-center z-5">
+        {list.map((item, index) => (
+          <li key={index} className="inline-block mx-2">
+            <NavLink
+              to={item.link}
+              className={({ isActive }) =>
+                isActive
+                  ? "text-white font-bold"
+                  : "text-gray-200 hover:text-white"
+              }
+            >
+              {item.name}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-8">
+      <p>
+        &copy; {new Date().getFullYear()} CENAMARK SHIPPING LINES. All rights
+        reserved.
+      </p>
+    </footer>
+  );
+};
--- src/routes/passengers.routes.tsx
+++ src/routes/passengers.routes.tsx
@@ -1,11 +1,18 @@
 import { lazy } from "react";
 
 const Passengers = lazy(() => import("../pages/passengers/index"));
+const PassengerLayout = lazy(() => import("../layout/passenger.layout"));
 
 const passengersRoutes = [
   {
     path: "/",
-    element: <Passengers />,
+    element: <PassengerLayout />,
+    children: [
+      {
+        path: "",
+        element: <Passengers />,
+      },
+    ],
   },
 ];
 
